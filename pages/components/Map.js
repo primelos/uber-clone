fb2691d82ed2ed27a19ff455b40af111ca08c03a
Ale <alearanibar@gmail.com>
2021-11-08 23:52:47 -0800

    uber project ready to deploy

---
+++ pages/components/Map.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import tw from "tailwind-styled-components";
+import mapboxgl from "!mapbox-gl";
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
+
+const Map = ({ pickupCoordinates, dropoffCoordinates }) => {
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-122.463, 37.7648],
+      zoom: 10,
+    });
+
+    if (pickupCoordinates) {
+      addToMap(map, pickupCoordinates);
+    }
+    if (dropoffCoordinates) {
+      addToMap(map, dropoffCoordinates);
+    }
+    if (pickupCoordinates && dropoffCoordinates) {
+      map.fitBounds([dropoffCoordinates, pickupCoordinates], {
+        padding: 60,
+      });
+    }
+  }, [pickupCoordinates, dropoffCoordinates]);
+
+  const addToMap = (map, coordinates) => {
+    const marker1 = new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
+  };
+
+  return <Wrapper id="map"></Wrapper>;
+};
+
+export default Map;
+
+const Wrapper = tw.div`
+ flex-1 h-1/2
+  
+`;
---
+++ pages/confirm.js
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from "react";
+import tw from "tailwind-styled-components";
+import Map from "./components/Map";
+import { useRouter } from "next/router";
+import RideSelector from "./components/RideSelector";
+import Link from "next/link";
+
+const token = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
+
+const Confirm = () => {
+  const [pickupCoordinates, setPickupCoordinates] = useState([0, 0]);
+  const [dropoffCoordinates, setDropoffCoordinates] = useState([0, 0]);
+  const router = useRouter();
+  const { pickup, dropoff } = router.query;
+
+  const getPickCoordinates = (pickup) => {
+    // if (!pickup) return;
+    fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup}.json?` +
+        new URLSearchParams({
+          access_token: token,
+          limit: 1,
+        })
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setPickupCoordinates(data.features[0].center);
+      });
+  };
+
+  const getDropoffCoordinates = (dropoff) => {
+    // if (!dropoff) return;
+    fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${dropoff}.json?` +
+        new URLSearchParams({
+          access_token: token,
+          limit: 1,
+        })
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setDropoffCoordinates(data.features[0].center);
+      });
+  };
+
+  useEffect(() => {
+    getPickCoordinates(pickup);
+    getDropoffCoordinates(dropoff);
+  }, [pickup, dropoff]);
+
+  return (
+    <Wrapper>
+      <ButtonContainer>
+        <Link href="/search">
+          <BackButton src="https://img.icons8.com/ios-filled/50/000000/left.png" />
+        </Link>
+      </ButtonContainer>
+
+      <Map
+        pickupCoordinates={pickupCoordinates}
+        dropoffCoordinates={dropoffCoordinates}
+      />
+      <RideContainer>
+        <RideSelector
+          pickupCoordinates={pickupCoordinates}
+          dropoffCoordinates={dropoffCoordinates}
+        />
+        <ConfirmButtonContainer>
+          <ConfirmButton>Confirm UberX</ConfirmButton>
+        </ConfirmButtonContainer>
+      </RideContainer>
+    </Wrapper>
+  );
+};
+
+export default Confirm;
+
+const Wrapper = tw.div`
+flex h-screen flex-col relative z-0
+`;
+
+const RideContainer = tw.div`
+  flex-1 flex-col flex h-1/2
+`;
+
+const ConfirmButtonContainer = tw.div`
+  border-t-2
+`;
+
+const ConfirmButton = tw.div`
+  bg-black text-white my-4 mx-4 py-4 text-center text-xl
+`;
+
+const ButtonContainer = tw.div`
+  left-4 absolute top-5 z-10 opacity-80
+`;
+
+const BackButton = tw.img`
+ h-full cursor-pointer  bg-gray-100 rounded-full pos shadow-md
+`;
